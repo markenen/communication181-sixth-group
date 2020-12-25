0de7fb6549b3361a46ebe247543ce045a619518f
hui-sama <76415430+hui-sama@users.noreply.github.com>
2020-12-25 08:13:18 +0800

    Update 2.1线性表问题（猴子选大王）.cpp

--- 201811050957/2.1线性表问题（猴子选大王）.cpp
+++ 201811050957/2.1线性表问题（猴子选大王）.cpp
@@ -1,1 +1,56 @@
-
+#include<iostream>
+#include<ostream>
+using namespace std;
+int getcount(int* p, int m, int n);
+int main()
+{
+    int* p;
+    int m, n, i;
+    cout << "猴子的数量 m 为:" << endl;
+    cin >> m;
+    cout << "数到退场的数 n 为:" << endl;
+    cin >> n;
+    p = (int*)malloc(sizeof(int) * m);
+    for (i = 0; i < m; i++)
+        p[i] = i + 1;
+    cout << "猴王的编号为:" << getcount(p, m, n) << endl;
+    free(p);
+    return 0;
+}
+int getcount(int* p, int m, int n)
+{
+    int i, temp, flag, count;
+    temp = 0;
+    count = m;
+    while (count != 1)
+    {
+        flag = 1;
+        while (flag < n)
+        {
+            if (temp >= m)
+                temp = 0;
+            if (p[temp] != 0)
+            {
+                flag++;
+            }
+            temp++;
+        }
+        if (temp >= m)
+            temp = 0;
+        while (temp < m && p[temp] == 0)
+        {
+            temp++;
+            if (temp >= m)
+                temp = 0;
+        }
+        p[temp] = 0;
+        count -= 1;
+    }
+    for (i = 0; i < m; i++)
+    {
+        if (p[i] != 0)
+        {
+            return p[i];
+        }
+    }
+}
