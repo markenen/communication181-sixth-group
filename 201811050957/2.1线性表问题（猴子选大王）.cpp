0b41b58e1092ba3142e5af9e08945f07583df683
hui-sama <76415430+hui-sama@users.noreply.github.com>
2021-01-05 23:57:54 +0800

    Update 2.1线性表问题（猴子选大王）.cpp

--- 201811050957/2.1线性表问题（猴子选大王）.cpp
+++ 201811050957/2.1线性表问题（猴子选大王）.cpp
@@ -1,56 +1,101 @@
+//链表法
 #include<iostream>
 #include<ostream>
+#include <iostream>
 using namespace std;
-int getcount(int* p, int m, int n);
+struct Monkey
+{
+    int num;  //猴子的编号
+    struct Monkey* next; //下一只猴子
+};
+
 int main()
 {
-    int* p;
-    int m, n, i;
-    cout << "猴子的数量 m 为:" << endl;
-    cin >> m;
-    cout << "数到退场的数 n 为:" << endl;
-    cin >> n;
-    p = (int*)malloc(sizeof(int) * m);
-    for (i = 0; i < m; i++)
-        p[i] = i + 1;
-    cout << "猴王的编号为:" << getcount(p, m, n) << endl;
-    free(p);
+    int m, n, i, j, king;
+    Monkey* head, * p1, * p2;
+    cin >> m >> n;
+    if (n == 1)
+    {
+        king = m;
+    }
+    else
+    {
+        //建立猴子围成的圆圈
+        p1 = p2 = new Monkey;
+        head = p1;
+        p1->num = 1;
+        for (i = 1; i < m; i++)  //其余m-1只猴子
+        {
+            p1 = new Monkey;  //p1是新增加的
+            p1->num = i + 1;
+            p2->next = p1;
+            p2 = p1;          //p2总是上一只
+        }
+        p2->next = head;      //最后一只再指向第一只，成了一个圆圈
+
+        //下面要开始数了
+        p1 = head;
+        for (i = 1; i < m; i++)  //循环m-1次，淘汰m-1只猴子
+        {
+            //从p1开始，数n-1只就找到第n只了
+            for (j = 1; j < n - 1; j++)  //实际先找到第n-1只，下一只将是被淘汰的
+                p1 = p1->next;    //围成圈的，可能再开始从第一只数，如果还未被淘汰的话
+
+            //找到了，
+            p2 = p1->next;  //p2将被删除
+            //cout<<"第"<<i<<"轮淘汰"<<p2->num<<endl;   //可以这样观察中间结果
+            p1->next = p2->next;  //p2就这样被“架空了”
+            p1 = p2->next;  //下一轮数数的新起点
+            delete p2;  //将不在链表中的结点放弃掉
+        }
+        king = p1->num;
+        delete p1;
+    }
+    cout << king << endl;
     return 0;
 }
-int getcount(int* p, int m, int n)
+
+
+
+
+//数组法
+#include <iostream>
+using namespace std;
+
+int main()
 {
-    int i, temp, flag, count;
-    temp = 0;
-    count = m;
-    while (count != 1)
+    int N, K = 1, M, p;
+    cin >> M >> N;
+    if (N == 1)
     {
-        flag = 1;
-        while (flag < n)
+        p = (M + 1);
+    }
+    else
+    {
+        int* a = new int[M + 1];
+        fill(a, a + M + 1, 0);
+
+        //num为约瑟夫环剩余人数，count为所报数
+        p = K;
+        int  count = 0, num = M;
+        while (num)
         {
-            if (temp >= m)
-                temp = 0;
-            if (p[temp] != 0)
+            //未出环的猴子参与报数，当count是N时出环
+            if (a[p] != -1 && ++count == N)
             {
-                flag++;
+                count = 0;
+                a[p] = -1; //将已出环的人标记为-1
+                num--;
+
             }
-            temp++;
-        }
-        if (temp >= m)
-            temp = 0;
-        while (temp < m && p[temp] == 0)
-        {
-            temp++;
-            if (temp >= m)
-                temp = 0;
-        }
-        p[temp] = 0;
-        count -= 1;
+
+            p++;                 //指针指向下一个人
+            p = (p - 1) % M + 1; //p范围转成[1,M]
+
+        };
     }
-    for (i = 0; i < m; i++)
-    {
-        if (p[i] != 0)
-        {
-            return p[i];
-        }
-    }
+
+    cout << "猴子大王是：" << p - 1 << " ";
+    return 0;
+
 }
